e20e5e8ed051d5a2a9862bb8edc83477669f2389
Josh Chandler <j5d1c7@gmail.com>
2020-10-03 20:20:39 -0400

    despitebike: basic steering

--- games/despitebike/main.py
+++ games/despitebike/main.py
@@ -13,6 +13,9 @@ import time
 char_template = cv2.imread("./character.png")
 viz = None
 
+def mid(a, b, c):
+    return min(max(a,b),max(b,c),max(a,c))
+
 def find_char(scr):
     match = cv2.matchTemplate(scr, char_template, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
@@ -32,7 +35,7 @@ def find_walls(scr, color, width, height):
     # scan line
     cv2.line(viz, (64 - width//2, height), (64 + width//2, height), (255, 0, 255), 1)
     
-    cv2.imshow("mask", mask)
+    cv2.imshow("mask_" + str(color), mask)
 
     x = 64 - width//2
     while x < 64 + width//2:
@@ -52,19 +55,37 @@ def find_walls(scr, color, width, height):
     return results
 
 def move(current_slot, walls):
-    if walls[current_slot]:
-        print("Danger!")
-    else:
-        # try to go to middle
-        if current_slot < 2:
-            Ui.press_key("Right")
-            Ui.release_key("Left")
-        elif current_slot > 2:
-            Ui.press_key("Left")
-            Ui.release_key("Right")
+    if walls[1][current_slot]:
+        # todo: more sophisticated ai...
+        d = current_slot - 2
+        if d >= 0:
+            if not walls[0][mid(0, current_slot-1, 4)]:
+                Ui.press_key("Left")
+                Ui.release_key("Right")
+            else:
+                Ui.press_key("Right")
+                Ui.release_key("Left")
         else:
-            Ui.release_key("Right")
-            Ui.release_key("Left")
+            if not walls[0][mid(0, current_slot+1, 4)]:
+                Ui.press_key("Right")
+                Ui.release_key("Left")
+            else:
+                Ui.press_key("Left")
+                Ui.release_key("Right")
+    else:
+        Ui.release_key("Right")
+        Ui.release_key("Left")
+        # try to go to middle
+        pass
+        # if current_slot < 2:
+        #     Ui.press_key("Right")
+        #     Ui.release_key("Left")
+        # elif current_slot > 2:
+        #     Ui.press_key("Left")
+        #     Ui.release_key("Right")
+        # else:
+        #     Ui.release_key("Right")
+        #     Ui.release_key("Left")
 
 w = Screen.find_pico8()
 Ui.set_window(w)
@@ -82,14 +103,14 @@ while True:
     current_slot = find_char(screen)
 
     # walls = find_walls(screen, Screen.COLOR_4, 72, 38)
-    walls = find_walls(screen, Screen.COLOR_A, 100, 24)
-    # walls = find_walls(screen, Screen.COLOR_F, 120, 20)
+    walls_0 = find_walls(screen, Screen.COLOR_F, 120, 20)
+    walls_1 = find_walls(screen, Screen.COLOR_A, 100, 24)
 
     cv2.circle(viz, (math.floor(current_slot*128/5 + 12), 90), 4, (255, 0, 255), 2)
     # cv2.imshow("screen", screen)
     cv2.imshow("viz", cv2.resize(viz, (512, 512), None, 0, 0, cv2.INTER_NEAREST))
 
-    move(current_slot, walls)
+    move(current_slot, [walls_0, walls_1])
 
     # 30 fps
     if cv2.waitKey(round(1000/30 - (time.time() - framestart))) & 0xFF == ord('q'):
